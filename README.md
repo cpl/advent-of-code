447742f80ad11191926f2d7cc2bd13bf67907ee5
Alexandru-Paul Copil <alexandru.p.copil@pm.me>
2018-12-04 16:26:10 +0000

    Add solution for day 3 part 2

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ Day  | Solutions | Problem
 0x02 | C         | [Link][p2]
 0x03 | C         | [Link][p3]
 
+Note: Solutions are not meant to be in optimal time or memory! My wish/goal for
+Advent of Code 2018 is to work the problems in as many languages as possible, depending
+on how much free time I have.
+
 [p1]: https://adventofcode.com/2018/day/3
 [p2]: https://adventofcode.com/2018/day/2
 [p3]: https://adventofcode.com/2018/day/1
--- day03/solution.c
+++ day03/solution.c
@@ -3,10 +3,20 @@
 
 
 #define MATRIX_SIZE 2000
+#define MAX_CLAIMS 1375
+
+typedef struct claim {
+    int id;
+    int x, y;
+    int h, w;
+
+    int overlap;
+} Claim;
 
 
 int main() {
-    int* matrix = calloc(MATRIX_SIZE*MATRIX_SIZE, sizeof(int));
+    int* matrix = calloc(MATRIX_SIZE*MATRIX_SIZE, sizeof(*matrix));
+    Claim claims[MAX_CLAIMS] = {0};
 
     // Create file pointer
     FILE* input;
@@ -17,11 +27,17 @@ int main() {
         exit(1);
     }
 
-
     // Read values while not EOF
     while(!feof(input)) {
         int id, x, y, h, w;
         fscanf(input, "#%d @ %d,%d: %dx%d\n", &id, &x, &y, &w, &h);
+
+        claims[id-1].id = id;
+        claims[id-1].x = x;
+        claims[id-1].y = y;
+        claims[id-1].w = w;
+        claims[id-1].h = h;
+
         for(int delta_y = 0; delta_y < h; delta_y++)
             for(int delta_x = 0; delta_x < w; delta_x++)
                 matrix[x+delta_x+(y+delta_y)*MATRIX_SIZE]++;
@@ -32,6 +48,20 @@ int main() {
         if(matrix[index] > 1)
             overlap++;
     }
+
+    for(int claim_index = 0; claim_index < MAX_CLAIMS; claim_index++) {
+        Claim c = claims[claim_index];
+        int claim_ok = 1;
+
+        for(int delta_y = 0; delta_y < c.h; delta_y++)
+            for(int delta_x = 0; delta_x < c.w; delta_x++)
+                if(matrix[c.x+delta_x+(c.y+delta_y)*MATRIX_SIZE] != 1)
+                    claim_ok = 0;
+
+        if(claim_ok)
+            printf("CLAIM ID: %d\n", c.id);
+    }
+
 
     printf("OVERLAPPING: %d\n", overlap);
 
