21bbb83945fb5409d13c1387b463e8b84230a3ba
Alexandru-Paul Copil <alexandru@cpl.li>
2020-12-04 08:50:42 +0200

    Add input caching to file

--- utils/getinput.go
+++ utils/getinput.go
@@ -22,20 +22,25 @@ func URL(year, day int) string {
 	return fmt.Sprintf("https://adventofcode.com/%d/day/%d/input", year, day)
 }
 
-
 func GetInput(year, day int) ([]byte, error) {
+	filename := fmt.Sprintf("./year%d/day%02d/input.txt", year, day)
+	data, err := ioutil.ReadFile(filename)
+	if err == nil {
+		return data, nil
+	}
+
 	req, err := http.NewRequest(http.MethodGet, URL(year, day), nil)
 	if err != nil {
 		return nil, fmt.Errorf("creating new request, %w", err)
 	}
 
 	req.AddCookie(&http.Cookie{
-		Name:       "session",
-		Value:      AuthCookie,
-		Path:       "/",
-		Domain:     ".adventofcode.com",
-		Secure:     true,
-		HttpOnly:   true,
+		Name:     "session",
+		Value:    AuthCookie,
+		Path:     "/",
+		Domain:   ".adventofcode.com",
+		Secure:   true,
+		HttpOnly: true,
 	})
 
 	res, err := http.DefaultClient.Do(req)
@@ -51,10 +56,12 @@ func GetInput(year, day int) ([]byte, error) {
 		return nil, fmt.Errorf("bad http status code, expected 200, got %d", res.StatusCode)
 	}
 
-	data, err := ioutil.ReadAll(res.Body)
+	data, err = ioutil.ReadAll(res.Body)
 	if err != nil {
 		return nil, fmt.Errorf("reading response body, %w", err)
 	}
+
+	_ = ioutil.WriteFile(filename, data, 0644)
 
 	return data, nil
 }
